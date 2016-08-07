69e067c204a68ad6a1178aa821717d7ee45dd9d8
Zhicheng Wang <zcwang@thoughtworks.com>
2016-08-07 09:35:25 +0800

    initial commit from seed

---
+++ Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularBBS.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
