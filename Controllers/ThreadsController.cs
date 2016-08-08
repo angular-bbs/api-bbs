1bc259eb8c5cfe684dc47269e171c67ba835e7fe
Zhicheng Wang <zcwang@thoughtworks.com>
2016-08-08 16:02:48 +0800

    test authorize

---
+++ Controllers/ThreadsController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AngularBBS.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class ThreadsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        [AllowAnonymous]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public object Post([FromBody]string value)
+        {
+            return new { req = value };
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
